9ae09ca00147b3cf9d3ea84e9254bc7d9157f841
trin <trin4ik@gmail.com>
2021-07-14 20:23:14 +0300

    * bugfix

--- src/lib/storage/DraftAsyncStorage.js
+++ src/lib/storage/DraftAsyncStorage.js
@@ -2,22 +2,25 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 class DraftAsyncStorage {
 
-    async get (key) {
-        const item = await AsyncStorage.getItem(key)
-        return JSON.parse(item)
-    }
+	async get (key) {
+		if (key) {
+			const item = await AsyncStorage.getItem(key)
+			return JSON.parse(item)
+		}
+		return false
+	}
 
-    async set (key, value) {
-        await AsyncStorage.setItem(key, JSON.stringify(value))
-    }
+	async set (key, value) {
+		if (key) await AsyncStorage.setItem(key, JSON.stringify(value))
+	}
 
-    async remove (key) {
-        await AsyncStorage.removeItem(key)
-    }
+	async remove (key) {
+		if (key) await AsyncStorage.removeItem(key)
+	}
 
-    async clear () {
-        await AsyncStorage.clear()
-    }
+	async clear () {
+		await AsyncStorage.clear()
+	}
 }
 
 export default DraftAsyncStorage
